495ae87ff066d066b140c7d0dff8d929b87d31ee
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-12-16 12:42:28 +0100

    named benches

--- benches/hash.rs
+++ benches/hash.rs
@@ -12,39 +12,38 @@ use poseidon_hash::poseidon_hash;
 use poseidon_hash::bn256::Bn256PoseidonParams;
 use poseidon_hash::specialization::specialization_macro::PosendonR2C1;
 
-#[bench]
-fn bench_rate_2(b: &mut test::Bencher) {
-    let params = Bn256PoseidonParams::new_checked_2_into_1();
-    let rng = &mut thread_rng();
-    let input = (0..2).map(|_| Fr::rand(rng)).collect::<Vec<_>>();
+// #[bench]
+// fn bench_rate_2(b: &mut test::Bencher) {
+//     let params = Bn256PoseidonParams::new_checked_2_into_1();
+//     let rng = &mut thread_rng();
+//     let input = (0..2).map(|_| Fr::rand(rng)).collect::<Vec<_>>();
 
-    b.iter(|| {
-        poseidon_hash::<Bn256>(&params, &input)
-    });
-}
+//     b.iter(|| {
+//         poseidon_hash::<Bn256>(&params, &input)
+//     });
+// }
 
-#[bench]
-fn bench_rate_specialized_2(b: &mut test::Bencher) {
-    let params = Bn256PoseidonParams::new_checked_2_into_1();
-    let rng = &mut thread_rng();
-    let input = (0..2).map(|_| Fr::rand(rng)).collect::<Vec<_>>();
+// #[bench]
+// fn bench_rate_specialized_2(b: &mut test::Bencher) {
+//     let params = Bn256PoseidonParams::new_checked_2_into_1();
+//     let rng = &mut thread_rng();
+//     let input = (0..2).map(|_| Fr::rand(rng)).collect::<Vec<_>>();
 
-    b.iter(|| {
-        let mut p = PosendonR2C1::<Bn256>::new(&params);
-        for &i in input.iter() {
-            p.absorb_single_value(i);
-        }
-        p.squeeze_out_single()
-    });
-}
+//     b.iter(|| {
+//         let mut p = PosendonR2C1::<Bn256>::new(&params);
+//         for &i in input.iter() {
+//             p.absorb_single_value(i);
+//         }
+//         p.squeeze_out_single()
+//     });
+// }
 
 #[bench]
 fn bench_rate_2_80_bits(b: &mut test::Bencher) {
     let rate = 2u32;
     let capacity = 1u32;
-    // total number of round is 39 as per https://toolkit.abdk.consulting/poseidon#parameters-calculator
     let num_full = 8u32;
-    let num_partial = 31u32;
+    let num_partial = 33u32;
     let security_level = 80u32;
     let params = Bn256PoseidonParams::new_for_params::<BlakeHasher>(capacity, rate, num_partial, num_full, security_level);
     let rng = &mut thread_rng();
@@ -55,14 +54,28 @@ fn bench_rate_2_80_bits(b: &mut test::Bencher) {
     });
 }
 
+#[bench]
+fn bench_rate_4_80_bits(b: &mut test::Bencher) {
+    let rate = 4u32;
+    let capacity = 1u32;
+    let num_full = 8u32;
+    let num_partial = 35u32;
+    let security_level = 80u32;
+    let params = Bn256PoseidonParams::new_for_params::<BlakeHasher>(capacity, rate, num_partial, num_full, security_level);
+    let rng = &mut thread_rng();
+    let input = (0..rate).map(|_| Fr::rand(rng)).collect::<Vec<_>>();
+
+    b.iter(|| {
+        poseidon_hash::<Bn256>(&params, &input)
+    });
+}
 
 #[bench]
 fn bench_rate_2_128_bits(b: &mut test::Bencher) {
     let rate = 2u32;
     let capacity = 1u32;
-    // total number of round is 61 as per https://toolkit.abdk.consulting/poseidon#parameters-calculator
     let num_full = 8u32;
-    let num_partial = 53u32;
+    let num_partial = 57u32;
     let security_level = 80u32;
     let params = Bn256PoseidonParams::new_for_params::<BlakeHasher>(capacity, rate, num_partial, num_full, security_level);
     let rng = &mut thread_rng();
@@ -74,12 +87,61 @@ fn bench_rate_2_128_bits(b: &mut test::Bencher) {
 }
 
 #[bench]
-fn bench_rate_10_120_bits(b: &mut test::Bencher) {
-    let rate = 10u32;
+fn bench_rate_4_128_bits(b: &mut test::Bencher) {
+    let rate = 4u32;
     let capacity = 1u32;
     let num_full = 8u32;
-    let num_partial = 88u32;
-    let security_level = 120u32;
+    let num_partial = 60u32;
+    let security_level = 80u32;
+    let params = Bn256PoseidonParams::new_for_params::<BlakeHasher>(capacity, rate, num_partial, num_full, security_level);
+    let rng = &mut thread_rng();
+    let input = (0..rate).map(|_| Fr::rand(rng)).collect::<Vec<_>>();
+
+    b.iter(|| {
+        poseidon_hash::<Bn256>(&params, &input)
+    });
+}
+
+#[bench]
+fn bench_rate_8_128_bits(b: &mut test::Bencher) {
+    let rate = 8u32;
+    let capacity = 1u32;
+    let num_full = 8u32;
+    let num_partial = 63u32;
+    let security_level = 80u32;
+    let params = Bn256PoseidonParams::new_for_params::<BlakeHasher>(capacity, rate, num_partial, num_full, security_level);
+    let rng = &mut thread_rng();
+    let input = (0..rate).map(|_| Fr::rand(rng)).collect::<Vec<_>>();
+
+    b.iter(|| {
+        poseidon_hash::<Bn256>(&params, &input)
+    });
+}
+
+#[bench]
+fn bench_rate_4_256_bits(b: &mut test::Bencher) {
+    let rate = 4u32;
+    let capacity = 2u32;
+    let num_full = 8u32;
+    let num_partial = 120u32;
+    let security_level = 256u32;
+    let params = Bn256PoseidonParams::new_for_params::<BlakeHasher>(capacity, rate, num_partial, num_full, security_level);
+    let rng = &mut thread_rng();
+    let input = (0..rate).map(|_| Fr::rand(rng)).collect::<Vec<_>>();
+
+    b.iter(|| {
+        poseidon_hash::<Bn256>(&params, &input)
+    });
+}
+
+
+#[bench]
+fn bench_rate_8_256_bits(b: &mut test::Bencher) {
+    let rate = 8u32;
+    let capacity = 2u32;
+    let num_full = 8u32;
+    let num_partial = 120u32;
+    let security_level = 256u32;
     let params = Bn256PoseidonParams::new_for_params::<BlakeHasher>(capacity, rate, num_partial, num_full, security_level);
     let rng = &mut thread_rng();
     let input = (0..rate).map(|_| Fr::rand(rng)).collect::<Vec<_>>();
